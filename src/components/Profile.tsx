52eec3ae953fe9fa14ad132d88e182fcaac4f051
Fernando Graciano <fernandoggraciano@gmail.com>
2021-02-25 01:44:19 -0300

    :zap: Adding leveling structure

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/fergracianoo.png" alt="Fernando Graciano" />
@@ -8,7 +13,7 @@ export function Profile() {
         <strong>Fernando Graciano</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
